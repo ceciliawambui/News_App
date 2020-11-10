70487bec578315631471cd455e7b708b009f486d
ceciliawambui <ceciliawambui026@gmail.com>
2020-11-10 17:40:01 +0300

    Added templates

--- news/views.py
+++ news/views.py
@@ -4,26 +4,50 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 import datetime as dt
-def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
 
+#def welcome(request):
+ #   return HttpResponse('Welcome to the Moringa Tribune')
 
 def news_of_day(request):
     date = dt.date.today()
-    #FUNCTION TO CONVERT DATE OBJEVCT TO FIND EXACT DAY
-    day = convert_dates(date)
-    html = "<html> <body> <h1> News for {day} {date.day}-{date.month}-{date.year}</h1> </body> </html>"
-
+    html = "today is {}-{}-{}".format(date.day, date.month, date.year)
     return HttpResponse(html)
 
-def convert_dates(dates):
-    #Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
-    days = ['Monday', 'Tuesday', 'Wednesday','Thursday','Friday', 'Saturday', 'Sunday']
+#def past_days_news(request,past_date):
+    # Converts data from the string Url
+    #date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
 
-    #Returning the actual day of week
-    day=days[day_number]
-    return day
+    #day = convert_dates(date)
+    #html = "News for {} {}-{}-{}".format(day, date.day, date.month, date.year)
+
+    #return HttpResponse(html)
+from django.shortcuts import render,redirect
+#......
+# View Function to present news from past days
+def past_days_news(request, past_date):
+
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(news_of_day)
+
+    return render(request, 'all-news/past-news.html', {"date": date})
+
+def news_of_day(request):
+    date = dt.date.today()
+    return render(request, 'all-news/today-news.html', {"date": date,})
+
+
+def welcome(request):
+    return render(request, 'welcome.html')
+
 
 
 # Create your views here.
