66eaa2ba37586b68d259e91d51cf48b23a6c37d8
ceciliawambui <ceciliawambui026@gmail.com>
2020-12-31 12:56:04 +0300

    Used Postgresql Database to store data in the app

--- news/models.py
+++ news/models.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Editor(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
